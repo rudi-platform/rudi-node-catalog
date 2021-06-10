93e397ec1f99b71da58745dd3a3dcb997a2c01e8
Olivier Martineau <485-omartineau@users.noreply.gitlab.univ-rennes1.fr>
2021-06-10 11:20:20 +0200

    accept report from Portal - tests

--- rudi-proxi/definitions/models/Report.js
+++ rudi-proxi/definitions/models/Report.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const mod = 'reportSch'
+
 // -----------------------------------------------------------------------------
 // External dependancies
 // -----------------------------------------------------------------------------
@@ -10,7 +12,7 @@ const _ = require('lodash')
 // -----------------------------------------------------------------------------
 // Internal dependancies
 // -----------------------------------------------------------------------------
-
+const log = require('../../utils/logging')
 const ids = require('../schemas/Identifiers')
 const api = require('../../config/confApi')
 
