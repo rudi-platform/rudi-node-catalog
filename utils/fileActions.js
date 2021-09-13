361b218cec0a3c50bf1f93f706e328601c19815a
Olivier Martineau <olivier.martineau@irisa.fr>
2021-09-13 15:18:16 +0200

    absolute path for profiles

--- utils/fileActions.js
+++ utils/fileActions.js
@@ -17,8 +17,8 @@ const utils = require('./jsUtils')
 exports.readIniFile = (confFile) => {
   const fun = 'readIniFile'
   try {
-    const fileContent = fs.readFileSync(`./${confFile}`, 'utf-8')
-    utils.consoleLog(mod, fun, `Conf file found at ./${confFile}`)
+    const fileContent = fs.readFileSync(`${confFile}`, 'utf-8')
+    utils.consoleLog(mod, fun, `Conf file found at ${confFile}`)
     const conf = ini.parse(fileContent)
     return conf
   } catch (err) {
