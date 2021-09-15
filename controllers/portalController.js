e03f245023ca4474300ce5fa6eb3c44ca5d06ad5
Olivier Martineau <olivier.martineau@irisa.fr>
2021-09-15 14:27:39 +0200

    bug signatureIsValid

--- controllers/portalController.js
+++ controllers/portalController.js
@@ -343,7 +343,7 @@ exports.checkSignatureWithPubKey = (accessToken) => {
     try {
       pubKeyPem = readFileSync(portal.PUBLIC_KEY, 'ascii')
     } catch (err) {
-      throw new (`The file with the Portal public key can't be accessed: ${err}`)
+      throw new `The file with the Portal public key can't be accessed: ${err}`()
     }
     let sslKey
     try {
@@ -354,10 +354,11 @@ exports.checkSignatureWithPubKey = (accessToken) => {
     // log.d(mod, fun, `sslKey: ${utils.beautify(sslKey)}`)
     // const keyName = sslKey.comment && sslKey.comment !== '(unnamed)' ? `'${sslKey.comment}' ` : ''
     // log.d(mod, fun, `${keyName}public key: ${sslKey.type} ${sslKey.size} bits`)
+    let signatureIsValid
     try {
       const verifier = sslKey.createVerify('sha256')
       verifier.update(`${jwtHeaderBase64url}.${jwtPayloadBase64url}`)
-      const signatureIsValid = verifier.verify(jwtSignatureBase64url, 'base64url')
+      signatureIsValid = verifier.verify(jwtSignatureBase64url, 'base64url')
     } catch (err) {
       throw new Error(`Error while verifying the Portal token signature: ${err}`)
     }
