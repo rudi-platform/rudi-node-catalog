5ea161840a0a8426463e91c2c3a98709c5a0f5ff
Olivier Martineau <olivier.martineau@irisa.fr>
2021-12-14 17:19:39 +0100

    portal token user

--- controllers/portalController.js
+++ controllers/portalController.js
@@ -431,8 +431,9 @@ exports.verifyPortalToken = (accessToken) => {
     // log.d(mod, fun, `JWT Portal payload: ${utils.beautify(jwtPayload)}`)
     // log.d(mod, fun, `JWT Portal user: ${jwtPortalUser}`)
     if (jwtPortalUser !== login) {
-      log.w(mod, fun, `Portal JWT: incorrect user: : ${jwtPortalUser}`)
-      throw new ForbiddenError(`Portal JWT: incorrect user`)
+      log.w(mod, fun, `Portal JWT: incorrect user: ${jwtPortalUser}`)
+      log.sysOnError(401, `Portal JWT: incorrect user: ${jwtPortalUser}, token=${accessToken†}`)
+      // throw new ForbiddenError(`Portal JWT: incorrect user`)
     }
     // if (jwtPayload[portal.JWT_CLIENT] !== login)
     //   throw new ForbiddenError('Portal JWT: incorrect client')
