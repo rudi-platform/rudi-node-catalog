ae5060770fe00f51613e2f3d47c750a4692ef4c5
Olivier Martineau <olivier.martineau@irisa.fr>
2022-09-05 14:50:11 +0200

    fix: remove curlirize import

--- utils/httpReq.js
+++ utils/httpReq.js
@@ -7,21 +7,21 @@ const mod = 'http'
 // import http from 'http'
 // import { curlirize } from 'axios-curlirize'
 import axios from 'axios'
-import AxiosCurlirize from 'axios-curlirize'
+// import AxiosCurlirize from 'axios-curlirize'
 // -------------------------------------------------------------------------------------------------
 // Internal dependecies
 // -------------------------------------------------------------------------------------------------
 import { USER_AGENT } from '../config/confApi.js'
-import { ENV_LOCAL } from '../config/appOptions.js'
+// import { ENV_LOCAL } from '../config/appOptions.js'
 import { beautify } from './jsUtils.js'
-import { getEnvironment } from '../controllers/sysController.js'
+// import { getEnvironment } from '../controllers/sysController.js'
 import { logD, logHttpAnswer, logT } from './logging.js'
 import { RudiError, BadRequestError } from './errors.js'
 
 // -------------------------------------------------------------------------------------------------
 // Debug axios
 // -------------------------------------------------------------------------------------------------
-if (getEnvironment() === ENV_LOCAL) AxiosCurlirize(axios)
+// if (getEnvironment() === ENV_LOCAL) AxiosCurlirize(axios)
 
 // -------------------------------------------------------------------------------------------------
 // Functions: header treatments
