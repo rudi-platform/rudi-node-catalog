020adc53e20b7db048d2150f781d23abf953f130
Olivier Martineau <olivier.martineau@irisa.fr>
2022-12-14 16:02:17 +0100

    fix: removing integration_error_id

--- controllers/metadataController.js
+++ controllers/metadataController.js
@@ -730,8 +730,9 @@ const updateMetadataState = async (dbMetadata, newState = StorageStatus.Online) 
     }
     if (metadata[API_INTEGRATION_ERROR_ID]) {
       delete metadata[API_INTEGRATION_ERROR_ID]
-      delete dbMetadata[API_INTEGRATION_ERROR_ID]
+      dbMetadata[API_INTEGRATION_ERROR_ID] = null
       await dbMetadata.save()
+      logD(mod, fun, 'Integration error flag removed')
     }
     logD(
       mod,
