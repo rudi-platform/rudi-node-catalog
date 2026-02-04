40d0cab2b115d7b5100f1e881c25c357e23af049
Olivier Martineau <olivier.martineau@irisa.fr>
2026-02-04 12:08:43 +0100

    fix: syslog level

--- src/config/confLogs.js
+++ src/config/confLogs.js
@@ -81,7 +81,8 @@ const SYSLOG_SECTION = 'syslog'
 
 const SYSLOG_APP_NAME = getConf(SYSLOG_SECTION, 'app_name', LOG_APP_NAME)
 
-const SYSLOG_LVL = getConf(SYSLOG_SECTION, 'log_level', 'info')
+const SYSLOG_LVL =
+  getConf(SYSLOG_SECTION, 'syslog_level') || getConf(SYSLOG_SECTION, 'log_level', 'info')
 // const SYSLOG_NODE_NAME = getConf(SYSLOG_SECTION, 'syslog_node_name')
 export const SYSLOG_PROTOCOL = getConf(SYSLOG_SECTION, 'syslog_protocol', 'unix')
 
